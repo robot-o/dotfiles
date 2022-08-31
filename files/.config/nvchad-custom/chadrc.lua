1ca863fe34520964a5eb168475750b91b2c547d4
robot-o <philp.dominik@gmail.com>
2022-08-31 10:34:21 +0200

    swayidle: restore brightness on resume, nvim: add treesitter languages to config

--- files/.config/nvchad-custom/chadrc.lua
+++ files/.config/nvchad-custom/chadrc.lua
@@ -12,4 +12,37 @@ M.ui = {
    theme = "onedark",
 }
 
+M.plugins = {
+  override = {
+    ["nvim-treesitter/nvim-treesitter"] = {
+      ensure_installed = {
+        "yaml",
+        "python",
+        "go",
+        "lua",
+        "c_sharp",
+        "c",
+        "cpp",
+        "dockerfile",
+        "glsl",
+        "graphql",
+        "hcl",
+        "html",
+        "css",
+        "javascript",
+        "json",
+        "jsonc",
+        "make",
+        "markdown",
+        "nix",
+        "rust",
+        "sql",
+      },
+    },
+  },
+  user = {
+    ["towolf/vim-helm"] = {},
+  }
+}
+
 return M
