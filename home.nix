cd0c06ad8a0a3fc754deff6aae404c6b02b61c25
robot-o <philp.dominik@gmail.com>
2022-11-21 21:31:02 +0100

    nixos: fortivpn

--- home.nix
+++ home.nix
@@ -87,6 +87,8 @@
       element-desktop
       # work
       super-productivity
+      networkmanager-fortisslvpn
+      openfortivpn
     ];
     file.".scripts".source = ./files/.scripts;
     file.".zshenv".source = ./files/.zshenv;
