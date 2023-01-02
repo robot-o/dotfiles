ff158e8e167d590988a34e7a44dfc341c1ef730f
robot-o <philp.dominik@gmail.com>
2023-01-02 10:55:11 +0100

    nvim: telescope: hidden file enable in picker

--- files/.config/nvim/after/plugin/telescope.lua
+++ files/.config/nvim/after/plugin/telescope.lua
@@ -1,4 +1,11 @@
 local ts = require('telescope.builtin')
+ts.setup {
+  pickers = {
+    find_files = {
+      hidden = true
+    }
+  }
+}
 vim.keymap.set('n', '<A-d>', ts.find_files, {})
 vim.keymap.set('n', '<A-f>', ts.live_grep, {})
 vim.keymap.set('n', '<A-b>', ts.buffers, {})
